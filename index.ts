879110b40aed321c5960400cf14a03971fd28115
An39nA0219 <an39na10282004@yahoo.co.jp>
2022-04-01 15:15:04 +0900

    スプレッド演算子について

--- index.ts
+++ index.ts
@@ -1,16 +1,22 @@
-// テンプレート文字列
-let template1 = `this is template strings.`;
-// 改行がそのまま保持される
-let template2 = `
-  Hello, TypeScript!
-  Thank you.
-`;
-console.log(template2);
+// スプレッド演算子
+let categories = ['post', 'new', 'info'];
 
-// 変数を埋め込んだ文字列
-let name = 'anna';
-template2 = `
-  Hello, TypeScript!
-  Thank you ${name}.
-`;
-console.log(template2);
+// 複製
+let copyCat = [...categories];
+console.log(copyCat, copyCat === categories);
+// ...を使うことで別の配列として展開することができる
+
+let pushCat = ['other', ...categories];
+console.log(pushCat);
+
+// オブジェクトも展開できる
+let post = { id: 1, content: 'dummy', created: '2020-01-01' };
+let copyObj = { ...post };
+console.log(copyObj);
+
+// オブジェクトを配列で展開することはできない
+// let test = [...post];
+
+// 配列をオブジェクトで展開することはできる。インデックス値がキーになる
+let test = { ...categories };
+console.log(test);
