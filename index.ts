4e5bcb04fa86a58cb459e7a4b6cdf854cec4a1e0
An39nA0219 <an39na10282004@yahoo.co.jp>
2022-04-01 15:27:36 +0900

    関数について

--- index.ts
+++ index.ts
@@ -1,32 +1,16 @@
-// 分割代入
-let categories = ['post', 'new', 'info', 'other'];
-let [a, b] = categories;
-console.log(a, b); //post, newが出てくる
+// 関数
+// 返り値がないときはvoidで書く。省略可能
+// 仮引数に?を入れてundefinedにすることもできる
+// 初期値の設定も可能
+function greet(name: string = 'anna'): void {
+  console.log(`Hello World ${name}`);
+}
+greet();
 
-// 1個飛ばしにもできる
-let [c, d, , f] = categories;
-console.log(c, d, f); //post, new, otherが出てくる
-
-// 一部をまとめる
-let [g, h, ...i] = categories;
-console.log(g, h, i); //post, new, [info,other]が出てくる
-
-// 変数の初期値の設定
-let [j = 10, k = 7] = [1];
-console.log(j, k); //1, 7が出てくる
-
-// オブジェクトも同様に
-// let post = { id: 1, content: 'dummy', created: '2020-01-01' };
-// let copyObj = { ...post };
-// console.log(copyObj);
-
-// const { id, content} = post;
-// console.log(id, content); //1, dummyが出てくる
-
-// 別名で取り出す キーの変数名で基本は取り出すのでこういう感じで書き換える
-let post = { id: 1, content: 'dummy', created: '2020-01-01' };
-let copyObj = { ...post };
-console.log(copyObj);
-
-const { id, content: body } = post;
-console.log(id, body); //1, dummyが出てくる
+// 可変長引数
+function sum(...values: number[]): number {
+  return values.reduce(function (prev, current) {
+    return prev + current;
+  });
+}
+console.log(sum(1, 3, 5, 9));
